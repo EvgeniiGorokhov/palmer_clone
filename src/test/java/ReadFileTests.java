e76bfa6051b8585bca45bdcb6b3d5f876814e5f5
Evgenii Gorokhov <sweetgor@gmail.com>
2022-07-16 21:39:57 +0600

    Выполниное задание

--- src/test/java/ReadFileTests.java
+++ src/test/java/ReadFileTests.java
@@ -1,9 +1,26 @@
 import org.junit.jupiter.api.Test;
 
+
 public class ReadFileTests {
 
     @Test
     public void secondLineShouldHabAlloha(){
-      //write here second task
+      boolean check = false;
+
+ 
+      try(FileReader reader = new FileReader("notes3.txt"))
+      {
+         // читаем посимвольно
+          int c;
+          while((c=reader.read())!=-1){
+               
+              System.out.print((char)c);
+          } 
+      }
+
+
+     org.junit.jupiter.api.Assertions.assertEquals(true,check);
+    
+      
     }
 }
